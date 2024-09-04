bbbcf54a1a63d97476d6e953867e7cbca78b5fa4
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-09-04 14:14:26 +0200

    wip

--- model/src/index.ts
+++ model/src/index.ts
@@ -18,7 +18,9 @@ export const platforma = BlockModel.create<BlockArgs>('Heavy')
   )
 
   .output('preset', (ctx) =>
-    ctx.prerun?.resolve({ field: 'preset', assertFieldType: 'Input' })?.getDataAsString()
+    ctx.prerun
+      ?.resolve({ field: 'preset', assertFieldType: 'Input', allowPermanentAbsence: true })
+      ?.getDataAsJson<any>()
   )
 
   .output('qc', (ctx) =>
@@ -34,19 +36,19 @@ export const platforma = BlockModel.create<BlockArgs>('Heavy')
   )
 
   .output('clones', (ctx) => {
-    // const collection = ctx.outputs
-    //   ?.resolve({ field: 'clones', assertFieldType: 'Input' })
-    //   ?.parsePObjectCollection();
-    // if (collection === undefined) return undefined;
-    // // if (collection === undefined || !collection.isComplete) return undefined;
-    // const pColumns = Object.entries(collection)
-    //   .map(([id, obj]) => obj)
-    //   .filter(isPColumn);
-    // return ctx.createPFrame(pColumns);
+    const collection = ctx.outputs
+      ?.resolve({ field: 'clones', assertFieldType: 'Input' })
+      ?.parsePObjectCollection();
+    if (collection === undefined) return undefined;
+    // if (collection === undefined || !collection.isComplete) return undefined;
+    const pColumns = Object.entries(collection)
+      .map(([id, obj]) => obj)
+      .filter(isPColumn);
+    return ctx.createPFrame(pColumns);
     // parseResourceMap(ctx.outputs?.resolve({ field: 'clones', assertFieldType: 'Input' }), (acc) =>
     //   acc.listInputFields()
     // )
-    return ctx.outputs?.resolve({ field: 'clones', assertFieldType: 'Input' })?.listInputFields();
+    // return ctx.outputs?.resolve({ field: 'clones', assertFieldType: 'Input' })?.listInputFields();
   })
 
   .output('inputOptions', (ctx) => {
--- ui/src/SettingsPage.vue
+++ ui/src/SettingsPage.vue
@@ -2,15 +2,46 @@
 import '@ag-grid-community/styles/ag-grid.css';
 import '@ag-grid-community/styles/ag-theme-quartz.css';
 
+import { platforma } from '@milaboratory/milaboratories.mixcr-clonotyping.model';
 import { useApp } from './app';
+import { PlDropdown } from '@milaboratory/platforma-uikit';
+import { computed } from 'vue';
+import { asyncComputed } from '@vueuse/core';
 
 const app = useApp();
 
 app.createArgsModel();
+
+const inputOptions = computed(() =>
+  app.getOutputFieldOkOptional("inputOptions")?.map((v) => ({
+    text: v.label,
+    value: v.ref,
+  }))
+);
+
+const args = app.createArgsModel();
+
+const presets = asyncComputed(async () => {
+  const presetsFile = app.getOutputFieldOkOptional("presets")
+  if (presetsFile === undefined)
+    return undefined;
+  return JSON.parse(new TextDecoder().decode(await platforma.blobDriver.getContent(presetsFile.handle)))
+})
+
+const presetOptions = computed(() => {
+  return presets.value?.map((preset: any) => ({ text: `${preset.label}${preset.vendor ? ' - ' + preset.vendor : ''}`, value: preset.presetName }))
+})
+
+const preset = computed(() => app.args.preset === undefined ? undefined : presets.value?.find((p: any) => p.presetName === app.args.preset))
+
 </script>
 
 <template>
-  <div class="container">Hi!</div>
+  <div class="container">
+    <pl-dropdown :options="inputOptions ?? []" v-model="args.model.input" label="Select dataset" clearable />
+    <pl-dropdown :options="presetOptions ?? []" v-model="args.model.preset" label="Select preset" clearable />
+    <pre>{{ preset }}</pre>
+  </div>
 </template>
 
 <style lang="css">
