e53c74a94221cbf41c581b596030e89b9ec384fd
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-10-07 19:11:33 +0300

    feat: progress parsing pattern

---
+++ model/src/progress.ts
@@ -0,0 +1,2 @@
+export const ProgressPattern =
+  /(?<stage>[^:]*)\:(?: *(?<progress>[0-9.]+)\%)?(?: *ETA\: *(?<eta>.+))?/;
--- test/assets/expected_results/mixcr.sh
+++ test/assets/expected_results/mixcr.sh
@@ -5,4 +5,4 @@ set -e
 mixcr exportPreset --preset-name milab-human-dna-xcr-7genes-multiplex -f preset.json
 
 # --add-step assembleContigs 
-mixcr analyze milab-human-dna-xcr-7genes-multiplex -f ../small_data_R1.fastq.gz ../small_data_R2.fastq.gz result
+mixcr analyze milab-human-dna-xcr-7genes-multiplex -f ../small_data_R1.fastq.gz ../small_data_R2.fastq.gz result > log.txt
