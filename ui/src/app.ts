9d270651df76502e140149c517872ec6f6ecaaad
Mariia Zueva <maryzueva@gmail.com>
2026-01-21 18:25:19 +0100

    Set default block label

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -26,6 +26,6 @@ const unwatch = watch(sdkPlugin, ({ loaded }) => {
   if (!loaded) return;
   const app = useApp();
   app.model.args.customBlockLabel ??= '';
-  app.model.args.defaultBlockLabel ??= 'Select Clonotype Definition';
+  app.model.args.defaultBlockLabel ??= 'Select Dataset';
   unwatch();
 });
